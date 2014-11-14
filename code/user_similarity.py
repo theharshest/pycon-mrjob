b5cc358a4279c1f4c6ad668937db1c2eef87bbfd
harshjha2006@gmail.com <harshjha2006@gmail.com>
2014-11-14 17:58:40 -0600

    Exercise 4

--- code/user_similarity.py
+++ code/user_similarity.py
@@ -1,25 +1,39 @@
 from mrjob.job import MRJob
 from mrjob.protocol import JSONValueProtocol
 
+def jaccard(xs, ys):
+    return float(len(set(xs) & set(ys))) / len(set(xs) | set(ys))
+
 class UserSimilarity(MRJob):
     INPUT_PROTOCOL = JSONValueProtocol
 
-    ###
-    # TODO: write the functions needed to
-    # 1) find potential matches, 
-    # 2) calculate the Jaccard between users, with a user defined as a set of
-    # reviewed businesses
-    ##/
+    def extract_biz_ids(self, _, record):
+        if record['type'] == 'review':
+            yield record['user_id'], record['business_id']
+
+    def combine_biz_ids(self, user_id, biz_ids):
+        yield user_id, list(set(biz_ids))
+
+    def biz_to_user(self, user_id, biz_ids):
+        for biz in biz_ids:
+            yield biz, (user_id, biz_ids)
+
+    def calculate_jaccard(self, biz_id, user_defs):
+        user_defs_list = list(user_defs)
+        for i, user_def in enumerate(user_defs_list):
+            for compare_def in user_defs_list[i+1:]:
+                score = jaccard(user_def[1], compare_def[1])
+                if score >= 0.5:
+                    yield (min(user_def[0], compare_def[0]), max(user_def[0],
+                            compare_def[0])), score
+
+    def unique_user(self, user_pair, scores):
+        yield user_pair, set(scores).pop()
 
     def steps(self):
-        """TODO: Document what you expect each mapper and reducer to produce:
-        mapper1: <line, record> => <key, value>
-        reducer1: <key, [values]>
-        mapper2: ...
-        """
-        return [self.mr(mapper=self.mapper1, reducer=self.reducer1),
-                self.mr(mapper=...)]
-
+        return [self.mr(mapper=self.extract_biz_ids, reducer=self.combine_biz_ids),
+                                self.mr(mapper=self.biz_to_user, reducer=self.calculate_jaccard),
+                                self.mr(reducer=self.unique_user)]
 
 if __name__ == '__main__':
     UserSimilarity.run()
